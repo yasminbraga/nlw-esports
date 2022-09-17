e42b7d0f19102d4abaaab8c87b6eaf2fecff3e78
yasminbraga <yasminbragateixeira@gmail.com>
2022-09-17 14:44:04 -0300

    add mobile app

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { GameController } from "phosphor-react-native";
+
+import { TouchableOpacity, View, Text } from "react-native";
+import { THEME } from "../../theme";
+import { DuoInfo } from "../DuoInfo";
+
+import { styles } from "./styles";
+
+export interface DuoCardProps {
+  id: string;
+  hourStart: string;
+  hourEnd: string;
+  name: string;
+  useVoiceChanner: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({ data, onConnect }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo label="Nome" value={data.name} />
+      <DuoInfo label="Tempo de Jogo" value={`${data.yearsPlaying} anos`} />
+      <DuoInfo
+        label="Disponibilidade"
+        value={`${data.weekDays.length} dias \u2022 ${data.hourStart} - ${data.hourEnd}`}
+      />
+      <DuoInfo
+        label="Chamada de áudio"
+        value={data.useVoiceChanner ? "Sim" : "Não"}
+        colorValue={
+          data.useVoiceChanner ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT
+        }
+      />
+
+      <TouchableOpacity style={styles.button} onPress={onConnect}>
+        <GameController color={THEME.COLORS.TEXT} size={20} />
+        <Text style={styles.buttonTitle}>Conectar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
